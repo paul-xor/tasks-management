3079a9e974c0b8f5a5d645cefd8e1f4747fcc1ec
Pavel Smolov <plexsmol@gmail.com>
2023-04-04 19:44:14 -0400

    feat: deleting a task

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -29,4 +29,12 @@ export class TasksService {
 
     return task;
   }
+
+  deleteTask(id: string): void {
+    const index = this.tasks.findIndex((item) => item.id === id);
+
+    if (index > -1) {
+      this.tasks.splice(index, 1);
+    }
+  }
 }
