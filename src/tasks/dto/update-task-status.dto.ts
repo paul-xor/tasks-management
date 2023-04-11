a40c0143e486432b81714459a60a54d2f0ad31a5
Pavel Smolov <plexsmol@gmail.com>
2023-04-11 17:59:08 -0400

    wip:...

--- src/tasks/dto/update-task-status.dto.ts
+++ src/tasks/dto/update-task-status.dto.ts
@@ -1,5 +1,5 @@
 import { IsEnum } from 'class-validator';
-import { TaskStatus } from '../tasks.model';
+import { TaskStatus } from '../task.model';
 
 export class UpdateTaskStatusDto {
   @IsEnum(TaskStatus)
