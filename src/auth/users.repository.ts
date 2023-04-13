9a2b00043ac7fc7d992d074e6c448e8a4ce88ca0
Pavel Smolov <plexsmol@gmail.com>
2023-04-12 21:33:49 -0400

    feat: feature, sign up

--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -1,9 +1,15 @@
 import { User } from './user.entity';
 import { DataSource, Repository } from 'typeorm';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 
 export class UsersRepository extends Repository<User> {
   constructor(private dataSource: DataSource) {
     super(User, dataSource.createEntityManager());
   }
-  //
+
+  async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    const { username, password } = authCredentialsDto;
+    const user = this.create({ username, password });
+    await this.save(user);
+  }
 }
