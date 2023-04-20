423f5cf3695538ad0bce8bf8d80c7b12afa1c6be
Pavel Smolov <plexsmol@gmail.com>
2023-04-19 20:10:21 -0400

    Revert "feat: feature, sign up"
    This reverts commit 9a2b00043ac7fc7d992d074e6c448e8a4ce88ca0.
    clean up some code

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
-import { AuthService } from './auth.service';
-import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import { Controller } from '@nestjs/common';
 
 @Controller('auth')
-export class AuthController {
-  constructor(private authService: AuthService) { }
-
-  @Post('/signUp')
-  signUp(@Body() authCredentialsDto: AuthCredentialsDto): Promise<void> {
-    return this.authService.signUp(authCredentialsDto);
-  }
-}
+export class AuthController {}
